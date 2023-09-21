75a20b2118cc4b604660c07725065204cd63f460
Doriano Benyocki <benyockid13@gmail.com>
2023-09-21 18:13:13 +0000

    Updated buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,14 @@
 #!/user/bin/env groovy
 
-import com.example.Docker
+/* import com.example.Docker
 def call(String imageName) {
     return new Docker(this).buildDockerImage(imageName)
+} */
+
+def buildImage() {
+echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push nanatwn/demo-app:jma-2.0'
 }
