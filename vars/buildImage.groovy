8016ea615be82b2e85f2f6809950f04e8b15e4ed
Doriano Benyocki <benyockid13@gmail.com>
2023-09-21 19:05:20 +0000

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -5,11 +5,11 @@ def call(String imageName) {
     return new Docker(this).buildDockerImage(imageName)
 } */
 
-def buildImage() {
+def call(String imageName) {
 echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-c0ntrl08', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t c0ntrl08/module8-java-maven-app-for-dockerinjenkins:jma-3.1 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push c0ntrl08/module8-java-maven-app-for-dockerinjenkins:jma-3.1'
+        sh "docker push $imageName"
     }
 }
